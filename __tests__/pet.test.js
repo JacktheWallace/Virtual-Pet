84fd8a6432cb154bbc5d9d054b8e70874ed70e89
Jack Wallace <jackwillamwallace@hotmail.com>
2023-02-28 14:21:16 +0000

    added test that will demonstrate that instances of pets are able to be given names

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,3 +5,11 @@ describe('constructor', () => {
         expect( new Pet('Fido')).toBeInstanceOf(Object);
     });
 });
+
+describe('pet naming', () => {
+    it('sets the name property of a pet', () => {
+        const pet = new Pet('Fido')
+
+        expect(pet.name).toEqual('Fido');
+    });
+});
