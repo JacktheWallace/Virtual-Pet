ffff5ba82065ce868d4198deb4d959e0332d38d2
Jack Wallace <jackwillamwallace@hotmail.com>
2023-02-28 14:41:36 +0000

    added test for pet aging, that it has an initial age and that this age can be increased by calling growUp()

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,3 +13,16 @@ describe('pet naming', () => {
         expect(pet.name).toEqual('Fido');
     });
 });
+
+describe('pet aging', () => {
+    it('returns a new pets initial age as 0', () => {
+        const pet = new Pet('Fido')
+
+        expect(pet.age).toEqual(0);
+    });
+    xit('incriments the pets age by 1 when growUp() is run', () => {
+        const pet = new Pet('Fido')
+        pet.growUp()
+        expect(pet.age).toEqual(1);
+    });
+});
